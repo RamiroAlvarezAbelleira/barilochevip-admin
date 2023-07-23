f8d0b07457809e6eb7a3177cfbf5ce17a21ad55c
RamiroAlvarezAbelleira <alvarezramiro1997@gmail.com>
2023-07-23 19:01:47 -0300

    created new login page

--- src/admin.js
+++ src/admin.js
@@ -5,11 +5,12 @@ import EquipmentPage from './pages/EquipmentPage';
 import CategoryPage from './pages/CategoryPage';
 import BrandPage from './pages/BrandPage';
 import BookingsPage from './pages/BookingsPage';
+import CustomLogin from './pages/LoginPage/login';
 
 const dataProvider = customDataProvider('http://[::1]:3000/api/v1');
 
 const App = () => (
-    <Admin dataProvider={dataProvider}>
+    <Admin dataProvider={dataProvider} loginPage={CustomLogin}>
       <Resource name="equipment" {...EquipmentPage} />
       <Resource name="categories" {...CategoryPage} />
       <Resource name="brands" {...BrandPage} />
---
+++ src/pages/LoginPage/login.js
@@ -0,0 +1,106 @@
+import { Avatar, Box, Button, Card, Container, TextField} from "@mui/material";
+import * as React from "react";
+import { useState } from "react";
+import { useLogin, useNotify, Notification } from "react-admin";
+import "./login.css"
+
+
+const CustomLogin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailInputError, setEmailInputError] = useState({
+    error: false,
+    errorMessage: "",
+  });
+  const [passwInputError, setPasswInputError] = useState({
+    error: false,
+    errorMessage: "",
+  });
+
+  const login = useLogin();
+  const notify = useNotify();
+
+
+  const passwIsValid = () => {
+    if (password.length >= 8) {
+      setPasswInputError({
+        error: false,
+        errorMessage: "",
+      });
+      return true;
+    } else if (password.length < 8) {
+      setPasswInputError({
+        error: true,
+        errorMessage: "You must provide a valid password",
+      });
+      return false;
+    }
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+    const passwIsOk = passwIsValid();
+    if (passwIsOk) {
+      login({ username: email, password }).catch(() =>
+        notify("Invalid email or password", { type: "error" })
+      );
+    }
+  };
+
+  return (
+    <div className='login-main'>
+      <Card className='login-card'>
+        <Box component="form" className='form-box' onSubmit={submit}>
+          <Container className='from-avatar-container'>
+            <Avatar className='from-avatar'>BV</Avatar>
+          </Container>
+          <TextField
+            name="email"
+            placeholder="Email"
+            className='form-input'
+            error={emailInputError.error}
+            helperText={emailInputError.errorMessage}
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+              setEmailInputError({
+                error: false,
+                errorMessage: "",
+              });
+            }}
+          />
+          <br />
+          <TextField
+            name="password"
+            type="password"
+            placeholder="Password"
+            className='form-input'
+            error={passwInputError.error}
+            helperText={passwInputError.errorMessage}
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+              setPasswInputError({
+                error: false,
+                errorMessage: "",
+              });
+            }}
+          />
+          <Button
+            fullWidth={false}
+            variant="contained"
+            color="primary"
+            size="medium"
+            type="submit"
+            className='form-button'
+          >
+            SIGN IN
+          </Button>
+        </Box>
+      </Card>
+      <Notification />
+    </div>
+  );
+};
+
+export default CustomLogin;
