0f50660a7fe3ad62421d8ce05a94dc0581a1daa3
RamiroAlvarezAbelleira <alvarezramiro1997@gmail.com>
2023-08-01 10:49:12 -0300

    EditEquipment fix

--- src/pages/EquipmentPage/EditEquipment.js
+++ src/pages/EquipmentPage/EditEquipment.js
@@ -10,8 +10,8 @@ const EditEquipment = (props) => {
             <TextInput fullWidth label='Precio' source='price' validate={[required("Campo requerido")]}/>
             <TextInput fullWidth label='Descripcion' source='description' validate={[required("Campo requerido")]}/>
             <TextInput fullWidth label='Stock Total' source='stock_total' validate={[required("Campo requerido")]}/>
-            <SelectInput fullWidth label='Categoria' source='category.id' choices={categories} validate={[required("Campo requerido")]} />
-            <SelectInput fullWidth label='Marca' source='marca.id' choices={marcas} validate={[required("Campo requerido")]} />
+            <SelectInput fullWidth label='Categoria' source='category_id' choices={categories} validate={[required("Campo requerido")]} />
+            <SelectInput fullWidth label='Marca' source='marca_id' choices={marcas} validate={[required("Campo requerido")]} />
             <ImageInput
             source="images"
             label="Imagen del Equipo"
